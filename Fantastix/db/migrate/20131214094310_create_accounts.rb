b96953632da161a46ce277f24843c62626a1adf6
Jurubita Razvan <jurubitarazvan@student.(none)>
2013-12-14 11:47:44 +0200

    Am creat tabelul Accounts #1

---
+++ Fantastix/db/migrate/20131214094310_create_accounts.rb
@@ -0,0 +1,15 @@
+class CreateAccounts < ActiveRecord::Migration
+  def change
+    create_table :accounts do |t|
+      t.integer :account_id
+      t.string :account_type
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.string :email
+      t.string :avatar
+
+      t.timestamps
+    end
+  end
+end
