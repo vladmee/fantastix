cf1adcef1bbc5aec2352c0a1f2ec20407bc1580e
Jurubita Razvan <jurubitarazvan@student.(none)>
2013-12-14 11:51:26 +0200

    Am creat tabelul Groups #1

--- Fantastix/config/routes.rb
+++ Fantastix/config/routes.rb
@@ -1,4 +1,8 @@
 Fantastix::Application.routes.draw do
+  resources :groups
+
+  resources :accounts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
