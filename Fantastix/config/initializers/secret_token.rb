59a908da9cf422772bd2288e883e780cf8dc212b
anka <anka@ubuntu.(none)>
2013-12-14 11:04:27 +0200

    Am creat aplicatia #3

---
+++ Fantastix/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fantastix::Application.config.secret_key_base = '57ef28d22f3744f98f7af419c519c60c818197c69133a2bfa2ed0e6b59a46a153e58cb8916315fa3dbcbd718aad9e265f2ef4764b4f08dbafc485f371cd47e4c'
---
+++ Fantastix/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fantastix::Application.config.session_store :cookie_store, key: '_Fantastix_session'
